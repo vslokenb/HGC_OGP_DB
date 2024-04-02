db3d485e9e6019edcc7b9b42eedeee953caecb16
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-04-01 22:00:52 -0400

    Update conn.py

--- read-write-ogp/postgres_tools/conn.py
+++ read-write-ogp/postgres_tools/conn.py
@@ -1,13 +1,20 @@
-import pwinput
-user_input = None
-def get_user_input():
-    global user_input
-    if user_input is None:
-        user_input = pwinput.pwinput(prompt='Enter user password: ', mask='*')
-    return user_input
-
-host='cmsmac04.phys.cmu.edu'
+host = 'localhost'
 database='hgcdb'
 user='postgres'
-# password='hgcal'
-password=get_user_input()
+password='hgcal'
+
+# import pwinput
+
+# def get_user_input():
+# #    global user_input
+#    # if user_input is None:
+#     user_input = pwinput.pwinput(prompt='Enter user password: ', mask='*')
+#     return user_input
+
+# global host
+# global database
+# global user
+# global password
+
+
+#password=get_user_input()
