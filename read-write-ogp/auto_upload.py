7b72995cda868910c4cdc126710e2f0b9d51e7da
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2024-08-14 13:53:48 -0400

    change structure of auto_upload.py

--- read-write-ogp/auto_upload.py
+++ read-write-ogp/auto_upload.py
@@ -1,36 +1,64 @@
 import time, os, subprocess
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+import argparse
 
-class NewFileHandler(FileSystemEventHandler):
-    def __init__(self, process_file_path):
-        self.process_file_path = process_file_path
+class FileUploader():
+    def __init__(self, checkdir, log_file = 'filelog.txt'):
+        self.log_file = log_file
+        self.checkdir = checkdir
+    
+    def __call__(self, action: 'str') -> None:
+        """Run the file uploader
+        
+        Parameters
+        - `action`: the path to the script to run on new files
+        """
+        if os.path.exists(self.log_file):
+            logged_files = self.read_log_file()
+        else: logged_files = []
 
-    def on_created(self, event):
-        if not event.is_directory:
-            print(event)
-            new_file_path = os.path.normpath(event.src_path).replace('\\','/')
-            print("\nNew file created:", new_file_path)
-            self.run_test_file(new_file_path)
+        curr_files = self.lst_dir()
+        new_files = [file for file in curr_files if file not in logged_files]
 
-    def run_test_file(self, new_file_path):
-        subprocess.Popen(['python', self.process_file_path, new_file_path])
+        if new_files:
+            print(f'New files detected: {new_files}')
+            self.rec_files(new_files, self.log_file)
+            self.run_on_new_files(new_files, action)
+        else:
+            print('No new files detected.')
+            
+    def read_log_file(self):
+        with open(self.log_file, 'r') as f:
+            return [line.strip() for line in f.readlines()]
 
-def monitor_directory(path, process_file_path):
-    event_handler = NewFileHandler(process_file_path)
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-        print("\nKeyboard interrupt received. Exiting gracefully.")
-    observer.join()
+    def lst_dir(self):
+        items = os.listdir(self.checkdir)
+        files = [item for item in items if os.path.isfile(os.path.join(self.checkdir, item))]
+        return files
+    
+    def rec_files(self, files, log_file):
+        with open(log_file, 'w') as f:
+            for file in files:
+                f.write(file + '\n')
 
-if __name__ == "__main__":
-    directory_to_watch = "C:/Users/Admin/Desktop/module_assembly_surveys/offsets/OGP_results"
-    process_file_path = "C:/Users/Admin/Desktop/module_assembly_surveys/offsets/data_processing_and_plotting/process_im.py"
+    def run_on_new_files(self, files, action):
+        """Run the action on each file in the list of files
+        
+        Parameters
+        - `files`: list of files to run the action on, without parent directory prefix."""
+        for file in files:
+            subprocess.Popen(['python', action, os.path.join(self.checkdir, file)])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Watch a directory for new files and process them.')
+    parser.add_argument('-d', '--directory', type=str, help='Directory to watch for new files', default="C:/Users/Admin/Desktop/module_assembly_surveys/offsets/OGP_results")
+    args = parser.parse_args()
+    
+    directory_to_watch = parser.d
+    print("============================================")
     print(f'Watching directory: {directory_to_watch}')
-    monitor_directory(directory_to_watch, process_file_path)
+
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    process_file_path = os.path.join(parent_dir, 'process_im.py')
+
+    uploader = FileUploader(directory_to_watch)
+    uploader(process_file_path)
