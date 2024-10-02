f3ba024e13ddc39023452cbc6028f3c8c5b4b720
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-10-02 12:00:51 -0400

    Comment out plot Agg

--- read-write-ogp/ogp_height_plotter.py
+++ read-write-ogp/ogp_height_plotter.py
@@ -2,7 +2,7 @@
 # import os, sys
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.colors as cls
 # import xlrd
