4b9cbf7b9fceb0b6d876a5fe2facc547a11999bc
Joy Zhou <yuntongzhou@outlook.com>
2024-12-09 13:38:39 -0500

    Add new metadata and template files for DummyPos1 component, including YAML, CSV, and text formats

---
+++ rwOGP/test.py
@@ -0,0 +1,14 @@
+import os, yaml, sys, json
+
+pjoin = os.path.join
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = pjoin(file_dir, 'rwOGP')
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
+
+from src.parse_data import DataParser
+
+if __name__ == '__main__':
+    parser = DataParser(pjoin('rwOGP', 'templates', 'dummy2.txt'), 'templates')
+    parser()
