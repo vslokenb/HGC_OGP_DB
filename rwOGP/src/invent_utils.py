57c4451c1cb8a493ff606e2d823fc1f34bdad315
Joy Zhou <yuntongzhou@outlook.com>
2025-01-28 14:09:52 -0500

    Enhance clear inventory functionality with user confirmation and improved help text

--- rwOGP/src/invent_utils.py
+++ rwOGP/src/invent_utils.py
@@ -1,6 +1,6 @@
 from src.config_utils import load_config
 from src.auto_upload import InventoryUpdater
-import json
+import json, sys
 
 def invent_print():
     """Print the current inventory."""
@@ -23,18 +23,21 @@ def clear_invent():
     else:
         invent_path = settings['inventory_path']
         print("=" * 90)
-        userinput = input("Enter the component type you want to clear: (e.g. baseplates) \n")
-        print(f"Clearing the current inventory {invent_path}...")
-
         inventory = InventoryUpdater.deal_corrupt(invent_path)
-
-        if userinput in inventory:
-            inventory[userinput] = []
-        else:
+        userinput = input("Enter the component type you want to clear: (e.g. baseplates) \n")
+        if userinput not in inventory:
             print("Component type not found in inventory.")
             print("The current component types are:")
             print(inventory.keys())
-            sys.exit(1) 
+            sys.exit(1)
+        else:
+            if_entire = input(f"Would you want to clear the entire inventory for this component type? (y/n) \n")
+            if if_entire == 'y':
+                print(f"Clearing the current inventory {invent_path}...")
+                inventory[userinput] = []
+            else:
+                print(f"These are the files in the inventory for {userinput}:")
+                print(inventory[userinput])
         
         with open(invent_path, 'w') as f:
             json.dump(inventory, f)
