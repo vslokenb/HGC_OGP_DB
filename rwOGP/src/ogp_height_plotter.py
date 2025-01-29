ccb13d931ea7956bd405572c4c6a87fd74b4c6d7
Joy Zhou <yuntongzhou@outlook.com>
2025-01-29 17:47:08 -0500

    Update DataParser input file and add validation checks for angle and offset calculations in PlotTool

--- rwOGP/src/ogp_height_plotter.py
+++ rwOGP/src/ogp_height_plotter.py
@@ -341,9 +341,8 @@ class PlotTool:
         print(f'Calculating Angle and Offsets with:  {HolePin} @: {HolePin_xy} & {SlotPin} @: {SlotPin_xy} \n')
         print(f"Geometry: {Geometry}; Density: {density}; PositionID: {PositionID}")
 
-        CenterOff, AngleOff, XOffset, YOffset = self.angle(HolePin_xy, SlotPin_xy, FD_points)
-        print(f"Assembly Survey X Offset: {XOffset:.3f} mm")
-        print(f"Assembly Survey Y Offset: {YOffset:.3f} mm")
+        CenterOff, AngleOff, XOffset, YOffset = angle(HolePin_xy, SlotPin_xy, FD_points, Geometry, density, PositionID)
+
         print(f"Assembly Survey Rotational Offset is {AngleOff:.5f} degrees")
         print(f"Assembly Survey Center Offset is {CenterOff:.3f} mm")
 
