Fix thickness calculation and logging in SurveyProcessor

rwOGP/src/process_survey.py
@@ -79,9 +79,10 @@ class SurveyProcessor():
             if report_thick is not None:
                 logging.info(f"Unconstrained thickness reported by OGP {report_thick} - Offset: {Offset}")
                 report_thick -= Offset
-            avg_thick = np.round(np.mean(plotter.z_points), 3) - Offset
-            logging.info(f"Thickness by averaging reported points after subtracting offset {Offset}: {avg_thick}")
-            db_upload.update({'flatness': np.round(metadata['Flatness'],3), 'thickness': avg_thick})
+            else:
+                report_thick = np.round(np.mean(plotter.z_points), 3) - Offset
+                logging.info(f"Thickness by averaging reported points after subtracting offset {Offset}: {report_thick}")
+            db_upload.update({'flatness': np.round(metadata['Flatness'],3), 'thickness': report_thick})
         elif singular_type == 'protomodule' or singular_type == 'module':
             XOffset, YOffset, AngleOff = plotter.get_offsets()
             db_upload.update({'x_offset_mu':np.round(XOffset*1000), 'y_offset_mu':np.round(YOffset*1000), 'ang_offset_deg':np.round(AngleOff,3),